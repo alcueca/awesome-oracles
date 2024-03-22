f259c59b063f70cf170a753fa581ca7e72e730bf
ruvaag <ruvaag@gmail.com>
2024-03-23 04:12:12 +0530

    fix: ASSET must be IERC20

--- src/erc4626/ERC4626Oracle.sol
+++ src/erc4626/ERC4626Oracle.sol
@@ -8,17 +8,17 @@ contract ERC4626Oracle {
     error OracleUnsupportedPair();
 
     IERC4626 public immutable VAULT;
-    address public immutable ASSET;
+    IERC20 public immutable ASSET;
 
     constructor(address _vault) {
         VAULT = IERC4626(_vault);
-        ASSET = VAULT.asset();
+        ASSET = IERC20(VAULT.asset());
     }
 
     function valueOf(address base, address quote, uint256 baseAmount) external view returns (uint256 quoteAmount) {
-        if (quote == address(VAULT) && base == ASSET) {
+        if (quote == address(VAULT) && base == address(ASSET)) {
             return VAULT.convertToShares(baseAmount);
-        } else if (quote == ASSET && base == address(VAULT)) {
+        } else if (quote == address(ASSET) && base == address(VAULT)) {
             return VAULT.convertToAssets(baseAmount);
         } else {
             revert OracleUnsupportedPair();
@@ -26,9 +26,9 @@ contract ERC4626Oracle {
     }
 
     function priceOf(address base, address quote) external view returns (uint256 baseQuotePrice) {
-        if (quote == address(VAULT) && base == ASSET) {
+        if (quote == address(VAULT) && base == address(ASSET)) {
             return VAULT.convertToShares(10 ** IERC20(ASSET).decimals());
-        } else if (quote == ASSET && base == address(VAULT)) {
+        } else if (quote == address(ASSET) && base == address(VAULT)) {
             return VAULT.convertToAssets(10 ** VAULT.decimals());
         } else {
             revert OracleUnsupportedPair();
