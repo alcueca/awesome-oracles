3c72c6a93d11c1cb79455af509c252ac23b18d84
ruvaag <ruvaag@gmail.com>
2024-03-23 05:12:30 +0530

    test: init

---
+++ test/ERC4626Oracle.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// contracts
+import {Test, console2} from "forge-std/Test.sol";
+import {ERC4626Oracle} from "../src/erc4626/ERC4626Oracle.sol";
+
+contract ERC4626OracleTest is Test {}
