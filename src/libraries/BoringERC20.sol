fd7196d85f81f663f2a06fc8c82c725120e5f4ef
ruvaag <ruvaag@gmail.com>
2024-04-09 18:19:32 +0530

    feat: erc4626 oracle (#1)
    * chore: forge init
    
    * forge install: forge-std
    
    v1.8.1
    
    * chore: pin tests to recent block
    
    * feat: init ERC4626Oracle
    
    * feat: erc4626 oracle
    
    * fix: revert with error for invalid calldata
    
    * perf: use constants
    
    * fix: ASSET must be IERC20
    
    * fix: base should precede quote
    
    * fix: use spec definition for priceOf
    
    * chore: move interfaces to common dir
    
    * feat: handle non-standard tokens with safeDecimals
    
    * feat: more comments
    
    * test: init
    
    * test: oracle setup tests
    
    * test: valueOf and priceOf tests
    
    * test: fix asset tests
    
    * fix: use interfaces from forge-std
    
    * feat: ERC4626Oracle implements IOracle
    
    * test: update pinned block for fork tests
    
    ---------
    
    Co-authored-by: Alberto Cuesta Cañada <38806121+alcueca@users.noreply.github.com>

---
+++ src/libraries/BoringERC20.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// With thanks to @BoringCrypto
+pragma solidity ^0.8.0;
+
+import "forge-std/interfaces/IERC20.sol";
+
+// solhint-disable avoid-low-level-calls
+
+library BoringERC20 {
+    bytes4 private constant SIG_DECIMALS = 0x313ce567; // decimals()
+
+    /// @notice Provides a safe ERC20.decimals version which returns '18' as fallback value.
+    /// @param token The address of the ERC-20 token contract.
+    /// @return (uint8) Token decimals.
+    function safeDecimals(IERC20 token) internal view returns (uint8) {
+        (bool success, bytes memory data) = address(token).staticcall(abi.encodeWithSelector(SIG_DECIMALS));
+        return success && data.length == 32 ? abi.decode(data, (uint8)) : 18;
+    }
+}
---
+++ test/ERC4626Oracle.t.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// types
+import {IERC20} from "forge-std/interfaces/IERC20.sol";
+import {IERC4626} from "forge-std/interfaces/IERC4626.sol";
+// libraries
+import {console2} from "forge-std/Test.sol";
+// contracts
+import {Test} from "forge-std/Test.sol";
+import {ERC4626Oracle} from "../src/erc4626/ERC4626Oracle.sol";
+
+contract ERC4626OracleTest is Test {
+    // erc4626 oracles
+    ERC4626Oracle yDaiOracle;
+    ERC4626Oracle yUsdcOracle;
+    ERC4626Oracle yWethOracle;
+
+    // yearn v3 vaults
+    address constant yDAI = 0x028eC7330ff87667b6dfb0D94b954c820195336c;
+    address constant yUSDC = 0xBe53A109B494E5c9f97b9Cd39Fe969BE68BF6204;
+    address constant yWETH = 0xc56413869c6CDf96496f2b1eF801fEDBdFA7dDB0;
+
+    // underlying vault tokens
+    address constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    // prices at block 19493590
+    uint256 constant DAI_yDAI = 999573011923275867; // DAI/yDAI
+    uint256 constant USDC_yUSDC = 999918000000000000; // USDC/yUSDC
+    uint256 constant WETH_yWETH = 999987596242017279; // WETH/yWETH
+
+    uint256 constant yDAI_DAI = 1000427170473423012; // yDAI/DAI
+    uint256 constant yUSDC_USDC = 1000081000000000000; // yUSDC/USDC
+    uint256 constant yWETH_WETH = 1000012403911837841; // yWETH/WETH
+
+    function setUp() public {
+        vm.createSelectFork("mainnet", block.number);
+        yDaiOracle = new ERC4626Oracle(yDAI);
+        yUsdcOracle = new ERC4626Oracle(yUSDC);
+        yWethOracle = new ERC4626Oracle(yWETH);
+    }
+
+    function testUnderlyingAsset() public view {
+        assertEq(address(yDaiOracle.ASSET()), DAI);
+        assertEq(address(yUsdcOracle.ASSET()), USDC);
+        assertEq(address(yWethOracle.ASSET()), WETH);
+    }
+
+    function testAssetScalar() public view {
+        // should be equal to the 10 ** underlying asset decimals
+        assertEq(yDaiOracle.ASSET_SCALAR(), 10 ** IERC20(DAI).decimals());
+        assertEq(yUsdcOracle.ASSET_SCALAR(), 10 ** IERC20(USDC).decimals());
+        assertEq(yWethOracle.ASSET_SCALAR(), 10 ** IERC20(WETH).decimals());
+    }
+
+    function testVaultScalar() public view {
+        // yearn vault shares mirror decimals for the underlying token
+        // dai and weth have 18 decimals, while usdc has 6 decimals
+        assertEq(yDaiOracle.VAULT_SCALAR(), 1e18);
+        assertEq(yUsdcOracle.VAULT_SCALAR(), 1e6);
+        assertEq(yWethOracle.VAULT_SCALAR(), 1e18);
+    }
+
+    function testPriceOfShares() public view {
+        // price of one vault share whole unit, in terms of the underlyin token
+        assertEq(yDaiOracle.priceOf(yDAI, DAI), yDAI_DAI);
+        assertEq(yUsdcOracle.priceOf(yUSDC, USDC), yUSDC_USDC);
+        assertEq(yWethOracle.priceOf(yWETH, WETH), yWETH_WETH);
+    }
+
+    function testPriceOfAssets() public view {
+        // price of one vault share whole unit, in terms of the underlyin token
+        assertEq(yDaiOracle.priceOf(DAI, yDAI), DAI_yDAI);
+        assertEq(yUsdcOracle.priceOf(USDC, yUSDC), USDC_yUSDC);
+        assertEq(yWethOracle.priceOf(WETH, yWETH), WETH_yWETH);
+    }
+
+    function testValueOfShares(uint256 daiAmt, uint256 usdcAmt, uint256 wethAmt) public view {
+        // value of vault share tokens in terms of underlying tokens
+        vm.assume(daiAmt <= IERC4626(yDAI).totalSupply());
+        vm.assume(usdcAmt <= IERC4626(yUSDC).totalSupply());
+        vm.assume(wethAmt <= IERC4626(yWETH).totalSupply());
+
+        assertEq(yDaiOracle.valueOf(yDAI, DAI, daiAmt), IERC4626(yDAI).convertToAssets(daiAmt));
+        assertEq(yUsdcOracle.valueOf(yUSDC, USDC, usdcAmt), IERC4626(yUSDC).convertToAssets(usdcAmt));
+        assertEq(yWethOracle.valueOf(yWETH, WETH, wethAmt), IERC4626(yWETH).convertToAssets(wethAmt));
+    }
+
+    function testValueOfAssets(uint256 daiAmt, uint256 usdcAmt, uint256 wethAmt) public view {
+        // value of underlying tokens in terms of vault share tokens
+        vm.assume(daiAmt <= IERC4626(yDAI).totalAssets());
+        vm.assume(usdcAmt <= IERC4626(yUSDC).totalAssets());
+        vm.assume(wethAmt <= IERC4626(yWETH).totalAssets());
+
+        assertEq(yDaiOracle.valueOf(DAI, yDAI, daiAmt), IERC4626(yDAI).convertToShares(daiAmt));
+        assertEq(yUsdcOracle.valueOf(USDC, yUSDC, usdcAmt), IERC4626(yUSDC).convertToShares(usdcAmt));
+        assertEq(yWethOracle.valueOf(WETH, yWETH, wethAmt), IERC4626(yWETH).convertToShares(wethAmt));
+    }
+}
