4242a0c8fa7baf20d4f28263e9e85bff93ac57bb
Anton Totomanov <63903313+totomanov@users.noreply.github.com>
2024-04-30 09:54:12 +0300

    feat: pair-only chainlink oracle (#9)
    * feat: pair-only chainlink oracle
    
    * feat: return fp18 quotes
    
    * docs: add natspec
    
    * chore: fmt

---
+++ src/chainlink/ChainlinkOracle.sol
@@ -0,0 +1,90 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// types
+import {IERC20} from "forge-std/interfaces/IERC20.sol";
+import {AggregatorV3Interface} from "./AggregatorV3Interface.sol";
+import {IOracle} from "../interfaces/IOracle.sol";
+import {BoringERC20} from "../libraries/BoringERC20.sol";
+
+contract ChainlinkOracle is IOracle {
+    using BoringERC20 for IERC20; // handles non-standard ERC20 tokens
+
+    AggregatorV3Interface public immutable FEED;
+    address public immutable NUMERATOR_ASSET;
+    address public immutable DENOMINATOR_ASSET;
+    uint256 public immutable MAX_STALENESS;
+
+    uint256 internal immutable NUMERATOR_SCALAR;
+    uint256 internal immutable DENOMINATOR_SCALAR;
+    uint256 internal immutable FEED_SCALAR;
+
+    constructor(address _feed, address _numeratorAsset, address _denominatorAsset, uint256 _maxStaleness) {
+        FEED = AggregatorV3Interface(_feed);
+        NUMERATOR_ASSET = _numeratorAsset;
+        DENOMINATOR_ASSET = _denominatorAsset;
+        MAX_STALENESS = _maxStaleness;
+
+        NUMERATOR_SCALAR = 10 ** IERC20(_numeratorAsset).safeDecimals();
+        DENOMINATOR_SCALAR = 10 ** IERC20(_denominatorAsset).safeDecimals();
+        FEED_SCALAR = 10 ** AggregatorV3Interface(_feed).decimals(); // decimals is not optional for the aggregator
+    }
+
+    /// @inheritdoc IOracle
+    function valueOf(address base, address quote, uint256 baseAmount) external view returns (uint256) {
+        bool isForward = _getQueryDirection(base, quote);
+        uint256 answer = _getAnswer(base, quote);
+
+        if (isForward) {
+            return (baseAmount * answer * 1e18) / (NUMERATOR_SCALAR * FEED_SCALAR);
+        } else {
+            return (baseAmount * 1e18 * FEED_SCALAR) / (answer * DENOMINATOR_SCALAR);
+        }
+    }
+
+    /// @inheritdoc IOracle
+    function priceOf(address base, address quote) external view returns (uint256) {
+        bool isForward = _getQueryDirection(base, quote);
+        uint256 answer = _getAnswer(base, quote);
+
+        if (isForward) {
+            return answer * 1e18 / FEED_SCALAR;
+        } else {
+            return FEED_SCALAR * 1e18 / answer;
+        }
+    }
+
+    /// @notice Fetch the latest price from Chainlink.
+    /// @param base The asset that the user needs to know the value for.
+    /// @param quote The asset in which the user needs to value the base.
+    /// @dev Reverts if answer is non-positive or price is too stale.
+    /// @return The latest Chainlink price.
+    function _getAnswer(address base, address quote) internal view returns (uint256) {
+        (, int256 answer,, uint256 updatedAt,) = FEED.latestRoundData();
+        if (answer <= 0) {
+            revert OracleUntrustedData(base, quote);
+        }
+
+        if (block.timestamp - updatedAt > MAX_STALENESS) {
+            revert OracleUntrustedData(base, quote);
+        }
+
+        return uint256(answer);
+    }
+
+    /// @notice Get the direction of a price query.
+    /// @param base The asset that the user needs to know the value for.
+    /// @param quote The asset in which the user needs to value the base.
+    /// @dev Reverts if base and quote do not correpsond to the feed assets.
+    /// @param return Whether if the query is in the direction of the feed.
+    function _getQueryDirection(address base, address quote) internal view returns (bool /* isForward */ ) {
+        if (base == NUMERATOR_ASSET && quote == DENOMINATOR_ASSET) {
+            return true;
+        } else if (base == DENOMINATOR_ASSET && quote == NUMERATOR_ASSET) {
+            return false;
+        } else {
+            // this oracle supports a particular chainlink feed
+            revert OracleUnsupportedPair(base, quote);
+        }
+    }
+}
---
+++ test/ChainlinkOracle.t.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+// types
+import {IERC20} from "forge-std/interfaces/IERC20.sol";
+// libraries
+import {console2} from "forge-std/Test.sol";
+// contracts
+import {Test} from "forge-std/Test.sol";
+import {ChainlinkOracle} from "../src/chainlink/ChainlinkOracle.sol";
+import {Denominations} from "../src/lib/Denominations.sol";
+
+contract ChainlinkOracleTest is Test {
+    // erc4626 oracles
+    ChainlinkOracle ethUsdOracle;
+    ChainlinkOracle usdcEthOracle;
+
+    address constant USDC_ETH_FEED = 0x986b5E1e1755e3C2440e960477f25201B0a8bbD4;
+    address constant ETH_USD_FEED = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
+
+    address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+    address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+    function setUp() public {
+        vm.createSelectFork("mainnet", 19702000);
+        ethUsdOracle = new ChainlinkOracle(ETH_USD_FEED, WETH, Denominations.USD, 24 hours);
+        usdcEthOracle = new ChainlinkOracle(USDC_ETH_FEED, USDC, WETH, 24 hours);
+    }
+
+    function testFeed() public view {
+        assertEq(address(ethUsdOracle.FEED()), ETH_USD_FEED);
+    }
+
+    function testNumeratorAsset() public view {
+        assertEq(ethUsdOracle.NUMERATOR_ASSET(), WETH);
+    }
+
+    function testDenominatorAsset() public view {
+        assertEq(ethUsdOracle.DENOMINATOR_ASSET(), Denominations.USD);
+    }
+
+    function testMaxStaleness() public view {
+        assertEq(ethUsdOracle.MAX_STALENESS(), 24 hours);
+    }
+
+    function testValueOfForward() public view {
+        assertApproxEqRel(usdcEthOracle.valueOf(USDC, WETH, 3200e6), 1e18, 0.05e18);
+        assertApproxEqRel(ethUsdOracle.valueOf(WETH, Denominations.USD, 2e18), 6400e18, 0.05e18);
+    }
+
+    function testValueOfReverse() public view {
+        assertApproxEqRel(usdcEthOracle.valueOf(WETH, USDC, 2e18), 6400e18, 0.05e18);
+        assertApproxEqRel(ethUsdOracle.valueOf(Denominations.USD, WETH, 3200e18), 1e18, 0.05e18);
+    }
+
+    function testPriceOfForward() public view {
+        assertApproxEqRel(usdcEthOracle.priceOf(USDC, WETH), 0.0003125e18, 0.05e18);
+        assertApproxEqRel(ethUsdOracle.priceOf(WETH, Denominations.USD), 3200e18, 0.05e18);
+    }
+
+    function testPriceOfReverse() public view {
+        assertApproxEqRel(usdcEthOracle.priceOf(WETH, USDC), 3200e18, 0.05e18);
+        assertApproxEqRel(ethUsdOracle.priceOf(Denominations.USD, WETH), 0.0003125e18, 0.05e18);
+    }
+}
